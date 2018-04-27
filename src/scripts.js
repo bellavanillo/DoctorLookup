d1de234d8c1f51a8cd22b6b9d69958f88c896b6b
bv <bellavanillo@gmail.com>
2018-04-27 10:54:58 -0700

    add error response and 200 response

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,17 @@ class medApi {
   apiCall() {
     return new Promise(function(resolve, reject) {
       let url = `https://developer.betterdoctor.com/admin/applications/1409617688777api_key=${process.env.exports.apiKey}`;
+      let request = new XMLHttpRequest();
 
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
     });
   }
 }
