84052e8dc0a07db4a130105372a87d79ff4ad643
bv <bellavanillo@gmail.com>
2018-04-27 13:52:28 -0700

    resolve 500 error message

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import $ from 'jquery';
 import { medApi } from './scripts.js';
 
 $(document).ready(function() {
-  $('form#symptoms').click(function(event) {
+  $('#button').click(function(event) {
     event.preventDefault();
     let medCall = new medApi();
     let promise = medCall.apiCall();
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      let results = body.data.practices.name;
-      $('.results').text(`'${body.data[0].practices[4].name}'`);
+      $('.results').append(`${body.data[4].name}`);
     }, function(error) {
       $('#errorText').text(`There was an error processing your request: ${error.message}`);
     });
