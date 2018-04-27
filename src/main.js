eb5249da888b01b08ba64e7350262aa1e554e4ad
bv <bellavanillo@gmail.com>
2018-04-27 16:21:48 -0700

    delete doctor.js, add address results

--- src/main.js
+++ src/main.js
@@ -16,8 +16,10 @@ $(document).ready(function() {
       let bodyArray = body.data;
       let name = body.data[0].practices[0].name;
       let newPatient = body.data[0].practices[0].accepts_new_patients;
+      let street = body.data[0].practices[0].visit_address.street;
+      let zip = body.data[0].practices[0].visit_address.zip;
       let number = body.data[0].practices[0].phones[0].number;
-      $('#results').append(`Name: ${name}, Phone number: ${number}, Is accepting new patients: ${newPatient}. `);
+      $('#results').append(`Name: ${name}, Phone number: ${number}, Is accepting new patients: ${newPatient}, Address: ${street}, ${zip}. `);
     }, function(error) {
       $('#errorText').text(`There was an error processing your request: ${error.message}`);
     });
