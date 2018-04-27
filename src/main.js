9a7760c55b334218771707eb302665c83eeea341
bv <bellavanillo@gmail.com>
2018-04-27 13:09:18 -0700

    receiving 500 error warning

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 import { medApi } from './scripts.js';
+
+$(document).ready(function() {
+  $('form#symptoms').click(function(event) {
+    event.preventDefault();
+    let medCall = new medApi();
+    let promise = medCall.apiCall();
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let results = body.data.practices.name;
+      $('.results').text(`'${body.data[0].practices[4].name}'`);
+    }, function(error) {
+      $('#errorText').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 class medApi {
   apiCall() {
     return new Promise(function(resolve, reject) {
-      let url = `https://developer.betterdoctor.com/admin/applications/1409617688777api_key=${process.env.exports.apiKey}`;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=97201&user_location=97201&skip=0&limit=10&user_key=b9cb7d0f19a88f5b697c88a77292e15c';
       let request = new XMLHttpRequest();
 
       request.onload = function() {
