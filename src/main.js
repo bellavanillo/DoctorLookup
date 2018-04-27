98a50a42c53eaf96e52ce080575fa9d714bec39b
bv <bellavanillo@gmail.com>
2018-04-27 14:57:44 -0700

    add broken for loop

--- src/main.js
+++ src/main.js
@@ -13,15 +13,16 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       let bodyArray =body.data;
-      let name = body.data[0].practices[0].name;
-      let newPatient = body.data[0].practices[0].accepts_new_patients;
-      let number = body.data[0].practices[0].phones[0].number;
-      // if (bodyArray.length === 0) {
-      //   $('#noresults').text(`There are no doctors found, please try again!`)
-      // } else {
-      //
-      // }
-      $('#results').append(`${body.data[0].practices[0].name}, ${body.data[0].practices[0].accepts_new_patients}, ${body.data[0].practices[0].phones[0].number}`);
+      let i;
+      for (i = 0, i < bodyArray.length; i++) {
+        let name = body.data[0].practices[0].name;
+        let newPatient = body.data[0].practices[0].accepts_new_patients;
+        let number = body.data[0].practices[0].phones[0].number;
+      }
+
+      $('#results').append(`Name: ${name}', number: ${number}, is accepting new patients: ${newPatient}. `);
+
+
     }, function(error) {
       $('#errorText').text(`There was an error processing your request: ${error.message}`);
     });
