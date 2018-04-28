35356477e54c10568d526aeef8acb5d670f50272
bv <bellavanillo@gmail.com>
2018-04-27 20:59:15 -0700

    fix for loop

--- src/main.js
+++ src/main.js
@@ -13,17 +13,18 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      let bodyArray = body.data;
-      let name = body.data[0].practices[0].name;
-      let newPatient = body.data[0].practices[0].accepts_new_patients;
-      let street = body.data[0].practices[0].visit_address.street;
-      let zip = body.data[0].practices[0].visit_address.zip;
-      let number = body.data[0].practices[0].phones[0].number;
-      for (let i = 0; i < bodyArray.length; i++){
-        $('#results').append(`Name: ${name}, Phone number: ${number}, Is accepting new patients: ${newPatient}, Address: ${street}, ${zip}. `);
-      }
-    }, function(error) {
-      $('#errorText').text(`There was an error processing your request: ${error.message}`);
+        if (body.data.length === 0) {
+          $('#noresults').show();
+        } else {
+          for (let i = 0; i < body.data.length; i++) {
+            $("#results1").append(`Name:${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
+            $("#results2").append(`Address:${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.zip},`);
+            $("#results3").append(` Number:${body.data[i].practices[0].phones[0].number}`);
+            $("#results4").append(`Is currently accepting new patients: ${body.data[i].practices[0].accepts_new_patients}`);
+            }
+        }
+      }, function(error) {
+        $('#errorText').text(`There was an error processing your request: ${error.message}`);
+      });
     });
   });
-});
